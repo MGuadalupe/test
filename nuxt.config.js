17b8fab0b9152b5a3353fc499a227f381cc6fd98
Mariel Vargas <marielguadalupevargas@gmail.com>
2021-03-08 19:13:13 -0300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'test-slider',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'uikit/dist/css/uikit.min.css',
+    'uikit/dist/css/uikit.css',
+    '~/assets/scss/styles.scss',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: ['~/plugins/uikit.js', '~/plugins/vueperslides'],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,179 @@
+<template>
+  <div>
+    <nav
+      class="uk-navbar uk-navbar-container uk-margin uk-position-absolute nav"
+    >
+      <div class="uk-navbar-right">
+        <a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
+      </div>
+    </nav>
+    <div class="uk-flex slider-columns">
+      <div class="uk-width-1-2@s">
+        <div
+          class="uk-height-1-1 uk-flex uk-flex-column uk-flex-middle uk-flex-center"
+        >
+          <div class="uk-width-3-4 text-container">
+            <div class="uk-flex">
+              <h3 class="indexSlider">
+                0{{ textIndex + 1 }}
+                <span>|</span>
+              </h3>
+              <img
+                :src="slider[textIndex].icon"
+                :alt="
+                  slider[textIndex].icon.substring(
+                    6,
+                    slider[textIndex].icon.indexOf('.')
+                  )
+                "
+                class="slide-icon"
+              />
+            </div>
+
+            <h2 class="title">{{ slider[textIndex].title }}</h2>
+            <hr class="hr" />
+            <p class="paragraph">
+              {{ slider[textIndex].paragraph }}
+            </p>
+            <div>
+              <button class="btn-primary">Saber más</button>
+            </div>
+          </div>
+          <div class="social-media uk-width-3-4 uk-visible@s">
+            <span uk-icon="facebook" class="uk-margin-right"></span>
+            <span uk-icon="twitter" class="uk-margin-right"></span>
+            <span uk-icon="instagram"></span>
+          </div>
+        </div>
+      </div>
+      <div class="slider uk-width-1-2@s">
+        <vueper-slides
+          ref="slider"
+          fixed-height="100vh"
+          :arrows="false"
+          :bullets="false"
+          class="slider"
+        >
+          <vueper-slide
+            v-for="(slide, indexSlider) in slider"
+            :key="indexSlider"
+            :image="slide.image"
+          />
+        </vueper-slides>
+      </div>
+    </div>
+    <div>
+      <button
+        class="chevron-slide-1"
+        @click="$refs.slider.previous(), slideImage()"
+      >
+        <span uk-icon="icon: chevron-left; ratio: 1.5"></span>
+      </button>
+      <button
+        class="chevron-slide-2"
+        @click="$refs.slider.next(), slideImage()"
+      >
+        <span uk-icon="icon: chevron-right; ratio: 1.5"></span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+import { gsap } from 'gsap'
+export default {
+  components: {
+    VueperSlides,
+    VueperSlide,
+  },
+  data: () => ({
+    textIndex: 0,
+
+    slider: [
+      {
+        icon: 'images/icons/1.svg',
+        title: 'Misión',
+        paragraph:
+          'El veganismo no es ninguna dieta. Es una actitud de rebeldía, una postura ética basada en el profundo respeto por los animales y nuestro entorno. Ese respeto pasa por no comerlos ni usarlos para vestir o esclavizarlos para nuestro disfrute o enriquecimiento. Es un espacio en el que se da voz a quienes no la tienen.',
+        image: 'images/hero-img-1.jpg',
+      },
+      {
+        icon: 'images/icons/2.svg',
+        title: 'Explora',
+        paragraph:
+          'The Flex component has an essential role in building layouts in UIkit. A lot of components, for example the Grid as well as horizontal navigations, like the Navbar, Subnav, Breadcrumb, Pagination, Tab and Dotnav are built with flexbox and can be used together with the utility classes from this component.',
+        image: 'images/hero-img-2.jpg',
+      },
+      {
+        icon: 'images/icons/3.svg',
+        title: 'Productos',
+        paragraph:
+          'The Zip file contains the compiled CSS and JavaScript files, which is everything you need to get started. Later, you might want to install and compile UIkit yourself and also create your own UIkit theme.',
+        image: 'images/hero-img-3.jpg',
+      },
+    ],
+  }),
+  mounted() {
+    this.slideImage()
+    gsap.fromTo(
+      '.social-media',
+      { y: 120 },
+      { duration: 0.8, y: 0, delay: 0.3 }
+    )
+    gsap.fromTo(
+      '.chevron-slide-2',
+      { y: 120 },
+      { duration: 0.8, y: 0, delay: 0.5 }
+    )
+    gsap.fromTo(
+      '.chevron-slide-1',
+      { y: 100 },
+      { duration: 0.8, y: 0, delay: 0.9 }
+    )
+    gsap.fromTo(
+      '.vueperslide:first-child',
+      { scale: 2, opacity: 0 },
+      { duration: 0.8, scale: 1, opacity: 1 }
+    )
+  },
+  methods: {
+    slideImage() {
+      this.textIndex = this.$refs.slider.slides.current
+      gsap.fromTo(
+        '.indexSlider',
+        { y: 50, opacity: 0 },
+        { duration: 0.8, y: 0, opacity: 1 }
+      )
+      gsap.fromTo(
+        '.slide-icon',
+        { y: 100, opacity: 0 },
+        { duration: 0.8, y: 0, opacity: 1, delay: 0.3 }
+      )
+      gsap.fromTo(
+        '.title',
+        { y: 150, opacity: 0 },
+        { duration: 0.8, y: 0, opacity: 1, delay: 0.5 }
+      )
+      gsap.fromTo(
+        '.hr',
+        { width: 0, opacity: 0 },
+        { duration: 0.8, width: '100%', opacity: 1, delay: 0.8 }
+      )
+      gsap.fromTo(
+        '.paragraph',
+        { y: 150, opacity: 0 },
+        { duration: 0.8, y: 0, opacity: 1, delay: 0.8 }
+      )
+      gsap.fromTo(
+        '.btn-primary',
+        { y: 150, opacity: 0 },
+        { duration: 0.8, y: 0, opacity: 1, delay: 0.8 }
+      )
+    },
+  },
+}
+</script>
+
+<style></style>
